3b2d0f5011ed18cf70b973c990a0783e6df1efce
oliverniel <olivernielikainen@gmail.com>
2025-01-27 13:21:24 +0200

    Toimii

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    post: process.env.PORT || 3000
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    }
   }
 })
