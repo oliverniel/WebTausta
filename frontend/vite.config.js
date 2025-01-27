46b27fd5340444ce7519e8629a10970e83e4a4fa
oliverniel <olivernielikainen@gmail.com>
2025-01-27 12:14:26 +0200

    config muutettu

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    post: process.env.PORT || 3000
+  }
 })
